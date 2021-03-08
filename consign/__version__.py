f58322043f28bf4e7384b416786931287c0a9220
jadra <jadraqueds@gmail.com>
2021-03-08 11:55:27 +0100

    v+=0.0.1: Successfully stores CSV

--- consign/__version__.py
+++ consign/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'consign'
 __description__ = 'Python storage for humans.'
 __url__ = 'https://requests.readthedocs.io'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __author__ = 'Daniel Jadraque'
 __author_email__ = 'jadraque@hey.com'
 __license__ = 'Apache 2.0'
