5b49fe3845634f8459f25a3967fe1fe743a85035
jadraque <d.jadraque@datary.io>
2021-03-19 13:37:52 +0100

    Added CSV creation test

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -6,7 +6,7 @@ from os import path, remove
 from os.path import isfile
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
-from consign.api import json
+from consign.api import csv, json
 from consign.adapters import StoreAdapter
 
 
@@ -23,6 +23,16 @@ class TestStringMethods(unittest.TestCase):
         self.provider = None
         self.connection_string = None
         self.container_name = None
+
+
+    def test_csv_creation(self):
+        '''
+        Tests that a CSV file gets created.
+        '''
+        data = []
+        test_path = './testing_csv.csv'
+        csv(data, test_path)
+        self.assertTrue(isfile(test_path))
 
 
     def test_json_creation(self):
@@ -54,6 +64,7 @@ class TestStringMethods(unittest.TestCase):
     def tearDown(self):
         try: remove('./testing_json.json')
         except: pass
-
+        try: remove('./testing_csv.csv')
+        except: pass
 
 TestRunner.main()
